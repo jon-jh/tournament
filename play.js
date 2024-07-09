40caab9483a536b69a9e9a24081b435a6110a6c8
jon-jh <hiebert456@gmail.com>
2024-07-09 11:07:43 -0600

    created a client for the snek game that connects locally

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+//set up the connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  }
+  );
+  //event handler
+  conn.on('connect', () => {
+    console.log('connected to the multiplayer snek game');
+  });
+  conn.on('data', (anyData) => {
+    console.log('message:', anyData);
+  });
+
+  //set up data interpreting for any incoming data(messages)
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+console.log('connecting...');
+connect();
+
