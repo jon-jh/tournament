f6e14e474bfab8c893851fffb7e2366f10fc2b15
jon-jh <hiebert456@gmail.com>
2024-07-09 12:15:31 -0600

    handle user input ctrl-c will now exit the game from the terminal screen

--- client.js
+++ client.js
@@ -1,7 +1,5 @@
-// connection handler
-
 const net = require('net');
-
+// connection handler
 //set up the connection with the game server
 const connect = function() {
   const conn = net.createConnection({
@@ -9,21 +7,26 @@ const connect = function() {
     port: 50541
   }
   );
+
   //event handler
+  //set up data interpreting for any incoming data(messages)
+  conn.setEncoding('utf8');
+  
   conn.on('connect', () => {
     console.log('connected to the multiplayer snek game');
-    conn.write('Name: GUY')
+    conn.write('Name: GUY');
     // testing to send up command non stop
-    setInterval(() => conn.write('Move: up'), 1000)
+    setInterval(() => conn.write('Move: up'), 1000);
   });
+  
   conn.on('data', (anyData) => {
     console.log('message:', anyData);
   });
 
-  //set up data interpreting for any incoming data(messages)
-  conn.setEncoding('utf8');
-
   return conn;
 };
 
-module.exports = connect;
+
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,5 +1,26 @@
-const connect = require('./client');
-
+const { connect } = require('./client');
 console.log('connecting...');
-connect();
 
+
+// setupInput
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  //below is how we handle user input, on data, run handleUserInput
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+//handleUserInput
+
+const handleUserInput = function(keyPress) {
+  if (keyPress === '\u0003') {
+    process.exit();
+  }
+};
+
+
+connect();
+setupInput();
